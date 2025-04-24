0a9b4ccec20674b53f408ec5150b88c80a2cbcb9
hbence97 <hegyesbence@gmail.com>
2025-04-24 02:41:39 +0200

    New neovim config

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,35 +1,40 @@
 return {
-	{
-		"catppuccin/nvim",
-		lazy = false,
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				flavour = "mocha",
-				-- vim.cmd.colorscheme("catppuccin")
-			})
-		end,
-	},
-	{
-		"sainnhe/gruvbox-material",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			-- Optionally configure and load the colorscheme
-			-- directly inside the plugin declaration.
-			vim.g.gruvbox_material_background = "hard" -- 'hard', 'medium', 'soft'
-			vim.g.gruvbox_material_foreground = "material" -- 'material', 'mix', 'original'
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        -- vim.cmd.colorscheme("catppuccin")
+      })
+    end,
+  },
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_background = "hard"  -- 'hard', 'medium', 'soft'
+      vim.g.gruvbox_material_foreground = "material" -- 'material', 'mix', 'original'
 
-			-- vim.cmd.colorscheme("gruvbox-material")
-		end,
-	},
-	{
-		"rose-pine/neovim",
-		lazy = false,
-		name = "rose-pine",
-		config = function()
-			vim.cmd.colorscheme("rose-pine")
-		end,
-	},
+      -- vim.cmd.colorscheme("gruvbox-material")
+    end,
+  },
+  {
+    "rose-pine/neovim",
+    lazy = false,
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup({
+        styles = {
+          italic = false,
+        },
+      })
+      vim.cmd.colorscheme("rose-pine")
+    end,
+  },
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,63 +1,86 @@
+local on_attach = function(_, bufnr)
+  local opts = {noremap = true, silent = true}
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+end
+
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-				settings = {
-					Lua = {
-						workspace = {
-							checkThirdParty = false,
-							telemetry = {
-								enable = false,
-							},
-							library = {
-								"${3rd}/love2d/library",
-							},
-						},
-						diagnostics = {
-							disable = { "lowercase-global", "param-type-mismatch" },
-						},
-					},
-				},
-			})
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.volar.setup({
-				capabilities = capabilities,
-				init_options = {
-					typescript = {
-						tsdk = "/Users/Bence/AppData/Local/nvim-data/mason/packages/vue-language-server/node_modules/typescript/lib",
-					},
-				},
-			})
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            workspace = {
+              checkThirdParty = false,
+              telemetry = {
+                enable = false,
+              },
+              library = {
+                "${3rd}/love2d/library",
+              },
+            },
+            diagnostics = {
+              disable = { "lowercase-global", "param-type-mismatch" },
+            },
+          },
+        },
+      })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.volar.setup({
+        capabilities = capabilities,
+        init_options = {
+          typescript = {
+            tsdk = "/Users/Bence/AppData/Local/nvim-data/mason/packages/vue-language-server/node_modules/typescript/lib",
+          },
+        },
+      })
+
+      lspconfig.emmet_ls.setup({
+        capabilities = capabilities,
+        filetypes = { "css", "html", "javascript", "javascriptreact", "sass", "scss", "typescriptreact" },
+        init_options = {
+          html = {
+            options = {
+-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+              ["bem.enabled"] = true,
+            },
+          },
+        }
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+
+    end,
+  },
 }
